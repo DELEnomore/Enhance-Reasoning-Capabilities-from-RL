43a157cdae4baed2d7faf86b4a7937b83fbf6f4b
BE <2321409910@qq.com>
2025-08-15 14:37:57 +0800

    1

--- run_eval.py
+++ run_eval.py
@@ -2,8 +2,12 @@ import sys
 
 from opencompass.cli.main import main
 
+from configs.base_config import GOOGLE_DRIVE_WORKSPACE_DIR
+
 sys.argv = [
     "main",
-    "configs/eval_config.py"
+    "configs/eval_config.py",
+    "--work-dir",
+    GOOGLE_DRIVE_WORKSPACE_DIR
 ]
 main()
