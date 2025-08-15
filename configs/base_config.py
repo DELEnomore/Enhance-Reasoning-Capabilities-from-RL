11f815375a321f4d7aee51dfb4fc3e4d24f18727
Be <53634370+DELEnomore@users.noreply.github.com>
2025-08-15 11:10:37 +0800

    Update base_config.py

--- configs/base_config.py
+++ configs/base_config.py
@@ -1,13 +1,12 @@
 
 from huggingface_hub import login
 
-GOOGLE_DRIVE_WORKSPACE_DIR = 'drive/MyDrive/colab_workspace/LLM'
-_CACHE_DIR = GOOGLE_DRIVE_WORKSPACE_DIR + '/cache'
-MODEL_CACHE_DIR = _CACHE_DIR + '/model'
-DATASET_CACHE_DIR = _CACHE_DIR + '/dataset'
+GOOGLE_DRIVE_WORKSPACE_DIR = './drive/MyDrive/colab_workspace'
+
+MODEL_CACHE_DIR = GOOGLE_DRIVE_WORKSPACE_DIR + '/models'
 MODEL_NAME = 'Qwen/Qwen2.5-1.5B-Instruct'
 
-MODEL_CHECKPOINT_DIR = GOOGLE_DRIVE_WORKSPACE_DIR + '/model_output/' + MODEL_NAME
+MODEL_CHECKPOINT_DIR = GOOGLE_DRIVE_WORKSPACE_DIR + '/finetune_models/' + MODEL_NAME
 MODEL_OUTPUT_DIR = MODEL_CHECKPOINT_DIR + '/best_model'
 
 login()
