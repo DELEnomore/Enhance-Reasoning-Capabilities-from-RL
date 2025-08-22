0a98a3cc180b98b8db848f98eb047f55e408400a
BE <2321409910@qq.com>
2025-08-22 11:40:32 +0800

    1

--- configs/eval_configs/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B/eval_config.py
+++ configs/eval_configs/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B/eval_config.py
@@ -14,11 +14,12 @@ models = [
         max_out_len=6000,
         batch_size=8,
         generation_kwargs=dict(
-          do_sample=True,
-          top_p=0.95,
-          temperature=0.6,
-          num_return_sequences=4,
-          repetition_penalty=1.1
+            do_sample=True,
+            top_p=0.95,
+            temperature=0.6,
+            # TODO 这个不支持>1吗？ acc结果为0
+            num_return_sequences=1,
+            repetition_penalty=1.1
           ),
         run_cfg=dict(num_gpus=1),
         pred_postprocessor=dict(type=extract_non_reasoning_content),
