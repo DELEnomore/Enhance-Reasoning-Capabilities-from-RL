3cacc702d88c86ef02caa47da676ab985c557f53
BE <2321409910@qq.com>
2025-08-25 09:37:34 +0800

    1

--- download_model.py
+++ download_model.py
@@ -2,7 +2,7 @@ from huggingface_hub import login
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from configs.base_config import MODEL_NAME, MODEL_DOWNLOAD_DIR
 
-login('')
+login()
 tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=MODEL_NAME)
 tokenizer.save_pretrained(MODEL_DOWNLOAD_DIR)
 model = AutoModelForCausalLM.from_pretrained(
