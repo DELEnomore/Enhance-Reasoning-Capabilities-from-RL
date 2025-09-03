212f54814194a1a6eea5777821bfd5114a52f7c4
BE <2321409910@qq.com>
2025-09-03 16:45:10 +0800

    1

---
+++ prepare_dataset.py
@@ -0,0 +1,22 @@
+from datasets import load_dataset
+from huggingface_hub import login
+
+from configs.base_config import DATASET_CACHE_DIR
+
+login('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')
+
+def batch_format_data(data, tokenizer):
+    problems = data['question']
+    answers = data['answer']
+    formated_data = {
+        'prompt':[
+            tokenizer.apply_chat_template(
+                [
+                    {"role": "user", "content": problem + "\nPlease reason step by step, and put your final answer within \\boxed{}."}
+                ], tokenize=False
+            )for problem, answer in zip(problems, answers)
+        ],
+        'answer': answers
+    }
+    return formated_data
+
