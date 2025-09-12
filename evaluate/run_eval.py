dc1de6c6952728e64f6d3499a8807eaa7d53bf77
BE <2321409910@qq.com>
2025-09-12 15:42:41 +0800

    1

--- evaluate/run_eval.py
+++ evaluate/run_eval.py
@@ -1,13 +1,6 @@
-import os
 import sys
-
 from opencompass.cli.main import main
-
 from configs.base_config import MODEL_NAME, EVAL_OUTPUT_DIR
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(current_dir)
-
 
 sys.argv = [
     "main",
