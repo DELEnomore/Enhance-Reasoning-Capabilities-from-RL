122ffeab39dd1b38bcb1ba43ba5dc15842905ce8
BE <2321409910@qq.com>
2025-09-12 10:22:29 +0800

    1

--- evaluate/run_eval.py
+++ evaluate/run_eval.py
@@ -2,12 +2,12 @@ import sys
 
 from opencompass.cli.main import main
 
-from configs.base_config import GOOGLE_DRIVE_WORKSPACE_DIR, MODEL_NAME
+from configs.base_config import MODEL_NAME, EVAL_OUTPUT_DIR
 
 sys.argv = [
     "main",
     f"../configs/eval_configs/{MODEL_NAME}/aime2025.py",
     "--work-dir",
-    GOOGLE_DRIVE_WORKSPACE_DIR
+    EVAL_OUTPUT_DIR
 ]
 main()
